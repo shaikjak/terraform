8b86224f505af43e0e3f68735051775648fd0af1
shaikjak <dudekulajakeer143@gmail.com>
2026-02-14 22:15:08 +0530

    docker file changes

---
+++ ec2.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "expense" {
+  name        = "expense"
+  description = "security group for expense"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Description = "expense instance"
+    CreatedBy  = "jakeer"
+  }
+}
+
+resource "aws_instance" "expense" {
+  instance_type          = "t2.micro"
+  ami                    = "ami-090252cbe067a9e58"  # Replace with valid AMI for your region
+  vpc_security_group_ids = [aws_security_group.expense.id]
+
+  tags = {
+    Name = "expense"
+  }
+}
