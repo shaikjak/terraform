247cb37a1a76b2051b16063c66d46cf2e227e454
shaikjak <dudekulajakeer143@gmail.com>
2026-02-15 15:34:05 +0530

    docker file changes

---
+++ variables/resource.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "expense" {
+  name        = var.expense_security_group.name
+  description = var.expense_security_group.description
+
+  ingress {
+    from_port   = var.ports
+    to_port     = var.ports
+    protocol    = var.protocols
+    cidr_blocks = var.cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  tags = {
+    Description = var.tags.name
+    CreatedBy   = var.tags.CreatedBy
+  }
+}
+
+resource "aws_instance" "expense" {
+  instance_type          = var.Instance_AMI.instance_type
+  ami                    =  var.Instance_AMI.ami # Replace with valid AvarMI for your region#
+  vpc_security_group_ids = [aws_security_group.expense.id]
+
+  tags = {
+    Name = "expense"
+  }
+}
