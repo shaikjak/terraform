752ee914985424fb3d34e4fde988a19a7c1253b1
shaikjak <dudekulajakeer143@gmail.com>
2026-02-16 21:55:14 +0530

    docker file changes

---
+++ data_source/data.tf
@@ -0,0 +1,14 @@
+data "aws_ami" "data-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
